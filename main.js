026e982fa229bcf640fd9ca641f413d576291550
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2023-01-05 14:42:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function preload() { }
+noseX = 0;
+noseY = 0;
+
+filter_img =  ""
+
+function preload() {
+    blue = loadImage("https://i.postimg.cc/59YfZg3f/glasses2.png")
+    glasses = loadImage("https://i.postimg.cc/L5zBJfzh/glasses.png")
+    mustache = loadImage("https://i.postimg.cc/rpnhTSJk/mustache.png")
+}
 
 function setup() {
     canvas = createCanvas(320, 280)
-    canvas.center();
+    canvas.position(525, 130);
 
     webcam = createCapture(VIDEO)
     webcam.size(320, 280)
@@ -14,6 +23,21 @@ function setup() {
 
 function draw() {
     image(webcam, 0, 0, 320, 280)
+
+    fill(171, 219, 255)
+    stroke(88, 184, 255)
+    circle(noseX, noseY, 10)
+
+    if(filter_img == "M") {
+        console.log("M")
+        image(mustache, noseX - 40, noseY - 5, 80, 40)
+    } else if (filter_img == "G") {
+        console.log("G")
+        image(glasses, noseX - 40, noseY - 35, 80, 40)
+    } else if (filter_img == "B") {
+        console.log("B")
+        image(blue, noseX - 40, noseY - 35, 80, 40)
+    }
 }
 
 function saveimg() {
@@ -27,9 +51,23 @@ function modelLoaded() {
 function gotPose(result) {
     if(result.length > 0) {
     console.log(result)
-    console.log("x = ", result[0].pose.nose.x)
-    console.log("y = ", result[0].pose.nose.y)
+    noseX = result[0].pose.nose.x;
+    noseY = result[0].pose.nose.y;
+    console.log("x = ", noseX)
+    console.log("y = ", noseY)
     } else {
-        window.alert("Error: cannot detect nose!")
+//        window.alert("Error: cannot detect nose!")
     }
+}
+
+function img1() {
+    filter_img = "M"
+}
+
+function img2() {
+    filter_img = "G"
+}
+
+function img3() {
+    filter_img = "B"
 }
